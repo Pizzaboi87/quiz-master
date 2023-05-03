13f15d72e8688670072e5364439e88ffc2c0dea9
Peter Weiser <peterweiser87@gmail.com>
2023-05-03 19:14:41 +0300

    added answer selection

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite selectedAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    int correctAnswerIndex;
 
     void Start()
     {
@@ -17,6 +22,28 @@ public class Quiz : MonoBehaviour
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        buttonImage = answerButtons[index].GetComponent<Image>();
+        buttonImage.sprite = selectedAnswerSprite;
+
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct answer!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = $"Sorry, the correct answer was\n{correctAnswer}";
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
         }
     }
 }
